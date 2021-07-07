bc3994fa5deb635ed9ddc995d83b22a3a3d24e29
Arsen Gharagyozyan <arsen.gharagyozyn.96@gmail.com>
2021-07-07 18:40:24 +0400

    First attempts

---
+++ telebarwdg/TelegramTaskbarWidget.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TeleBarWdg
+{
+    public partial class TelegramTaskbarWidget : UserControl
+    {
+        public TelegramTaskbarWidget()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TelegramWindow window = new TelegramWindow();
+            window.Show();
+            var globalPos = button1.PointToScreen(Point.Empty);
+            window.Location =
+                new Point(globalPos.X - window.Width / 2 + button1.Width / 2,
+                globalPos.Y - window.Height - 10);
+        }
+    }
+}
